a6b50889fc49efd1726a545c7a97e65cff4e2249
Lukas Samuolis <samuolis.lu@gmail.com>
2017-07-06 15:56:19 +0300

    Last Update for this time
    Removed unused files and fixed bugs

--- app/src/main/java/com/example/android/myquiz/MainActivity.java
+++ app/src/main/java/com/example/android/myquiz/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.android.myquiz;
 
-
-import android.content.Context;
 import android.content.DialogInterface;
-import android.provider.MediaStore;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -18,23 +14,19 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import static android.R.color.transparent;
 import static com.example.android.myquiz.R.id.group1;
 import static com.example.android.myquiz.R.id.group2;
 import static com.example.android.myquiz.R.id.group3;
 import static com.example.android.myquiz.R.id.group4;
 import static com.example.android.myquiz.R.id.group5;
 import static com.example.android.myquiz.R.id.group6;
-
 import static com.example.android.myquiz.R.id.q1a1;
 import static com.example.android.myquiz.R.id.q2a2;
 import static com.example.android.myquiz.R.id.q3a1;
 import static com.example.android.myquiz.R.id.q4a3;
 import static com.example.android.myquiz.R.id.q5a3;
 import static com.example.android.myquiz.R.id.q6a3;
-
-
-
+import static com.example.android.myquiz.R.id.q8a2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
     public RadioGroup jgroup4;
     public RadioGroup jgroup5;
     public RadioGroup jgroup6;
-    public RadioGroup jgroup7;
     public RadioButton q1a1j;
     public RadioButton q2a2j;
     public RadioButton q3a1j;
@@ -54,12 +45,10 @@ public class MainActivity extends AppCompatActivity {
     public CheckBox q8a1j;
     public CheckBox q8a2j;
     public CheckBox q8a3j;
+    public CheckBox q8a4j;
     public EditText q7a;
     int answer2;
     EditText editText1;
-
-    final Context c = this;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
         jgroup4 = (RadioGroup) findViewById(group4);
         jgroup5 = (RadioGroup) findViewById(group5);
         jgroup6 = (RadioGroup) findViewById(group6);
-
         q1a1j = (RadioButton) findViewById(q1a1);
         q2a2j = (RadioButton) findViewById(q2a2);
         q3a1j = (RadioButton) findViewById(q3a1);
@@ -81,44 +69,51 @@ public class MainActivity extends AppCompatActivity {
         q6a3j = (RadioButton) findViewById(q6a3);
         q7a = (EditText) findViewById(R.id.q7a);
         q8a1j = (CheckBox) findViewById(R.id.q8a1);
-        q8a2j = (CheckBox) findViewById(R.id.q8a2);
+        q8a2j = (CheckBox) findViewById(q8a2);
         q8a3j = (CheckBox) findViewById(R.id.q8a3);
-
+        q8a4j = (CheckBox) findViewById(R.id.q8a4);
 
     }
-
 
     //This is what i get after hitting submit button
 
     public void submit(View view) {
 
-        editText1=(EditText) findViewById(R.id.editText);
+        editText1 = (EditText) findViewById(R.id.editText);
         int score = 0;
         if ((q1a1j.isChecked())) {
-            score = score + 1;}
+            score = score + 1;
+        }
         if ((q2a2j.isChecked())) {
-            score = score + 1;}
+            score = score + 1;
+        }
         if ((q3a1j.isChecked())) {
-            score = score + 1;}
+            score = score + 1;
+        }
         if ((q4a3j.isChecked())) {
-            score = score + 1;}
+            score = score + 1;
+        }
         if ((q5a3j.isChecked())) {
-            score = score + 1;}
+            score = score + 1;
+        }
         if ((q6a3j.isChecked())) {
-            score = score + 1;}
-        if ((q7a.getText().toString().equals("Pythagorean")||q7a.getText().toString().equals("pythagorean"))) {
-            score = score + 1;}
-        if ((q8a3j.isChecked()&&q8a1j.isChecked()&&q8a2j.isChecked())) {
-            score = score + 1;}
-        double answer = (score / 8.)* 100;
+            score = score + 1;
+        }
+        if ((q7a.getText().toString().equals("Pythagorean") || q7a.getText().toString().equals("pythagorean"))) {
+            score = score + 1;
+        }
+        if ((q8a3j.isChecked() && q8a1j.isChecked() && q8a2j.isChecked() && !q8a4j.isChecked())) {
+            score = score + 1;
+        }
+        double answer = (score / 8.) * 100;
         answer2 = (int) Math.floor(answer);
 
         // This is dialog code, it need a bit practice with him to understand
 
-        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(this);
         View mView = layoutInflaterAndroid.inflate(R.layout.result, null);
-        if (jgroup1.getCheckedRadioButtonId()!=-1&&jgroup2.getCheckedRadioButtonId()!=-1&& jgroup3.getCheckedRadioButtonId()!=-1&&jgroup4.getCheckedRadioButtonId()!=-1
-                &&jgroup5.getCheckedRadioButtonId()!=-1&&jgroup6.getCheckedRadioButtonId()!=-1&&q7a.getText().toString()!="") {
+        if (jgroup1.getCheckedRadioButtonId() != -1 && jgroup2.getCheckedRadioButtonId() != -1 && jgroup3.getCheckedRadioButtonId() != -1 && jgroup4.getCheckedRadioButtonId() != -1
+                && jgroup5.getCheckedRadioButtonId() != -1 && jgroup6.getCheckedRadioButtonId() != -1 && !q7a.getText().toString().equals("")) {
             TextView dialog1 = (TextView) mView.findViewById(R.id.dialogTitle);
             dialog1.setText("");
             ImageView image1 = (ImageView) mView.findViewById(R.id.resultImage);
@@ -127,70 +122,60 @@ public class MainActivity extends AppCompatActivity {
             else
                 image1.setImageResource(R.drawable.sad3);
         }
-        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(this);
         alertDialogBuilderUserInput.setView(mView);
         alertDialogBuilderUserInput.setCancelable(true);
-        if (jgroup1.getCheckedRadioButtonId()!=-1&&jgroup2.getCheckedRadioButtonId()!=-1&& jgroup3.getCheckedRadioButtonId()!=-1&&jgroup4.getCheckedRadioButtonId()!=-1
-                &&jgroup5.getCheckedRadioButtonId()!=-1&&jgroup6.getCheckedRadioButtonId()!=-1&&q7a.getText().toString()!=""){
-        alertDialogBuilderUserInput.setPositiveButton("Check Answers", new DialogInterface.OnClickListener() {
+        if (jgroup1.getCheckedRadioButtonId() != -1 && jgroup2.getCheckedRadioButtonId() != -1 && jgroup3.getCheckedRadioButtonId() != -1 && jgroup4.getCheckedRadioButtonId() != -1
+                && jgroup5.getCheckedRadioButtonId() != -1 && jgroup6.getCheckedRadioButtonId() != -1 && !q7a.getText().toString().equals("")) {
+            alertDialogBuilderUserInput.setPositiveButton("Check Answers", new DialogInterface.OnClickListener() {
 
-                    public void onClick(DialogInterface dialogBox, int id) {
-                        dialogBox.cancel();
-                        checkAnswers();
-
-
-                    }
-                });
-        alertDialogBuilderUserInput.setNegativeButton("Try again",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogBox, int id) {
-                                dialogBox.cancel();
-                                reset();
-                            }
-                        });}
-        else
-        {alertDialogBuilderUserInput.setNegativeButton("Back",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialogBox, int id) {
-                        dialogBox.cancel();
-                    }
-                });}
-
-
-
+                public void onClick(DialogInterface dialogBox, int id) {
+                    dialogBox.cancel();
+                    checkAnswers();
+                }
+            });
+            alertDialogBuilderUserInput.setNegativeButton("Try again",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogBox, int id) {
+                            dialogBox.cancel();
+                            reset();
+                        }
+                    });
+        } else {
+            alertDialogBuilderUserInput.setNegativeButton("Back",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogBox, int id) {
+                            dialogBox.cancel();
+                        }
+                    });
+        }
         AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
         alertDialogAndroid.show();
     }
-
-
-
-
 
     //this function adds green background to my answers
 
     public void checkAnswers() {
 
-                q1a1j.setBackgroundResource(R.drawable.good_answer);
-                q2a2j.setBackgroundResource(R.drawable.good_answer);
-                q3a1j.setBackgroundResource(R.drawable.good_answer);
-                q4a3j.setBackgroundResource(R.drawable.good_answer);
-                q5a3j.setBackgroundResource(R.drawable.good_answer);
-                q6a3j.setBackgroundResource(R.drawable.good_answer);
-                q8a1j.setBackgroundResource(R.drawable.good_answer);
-                q8a2j.setBackgroundResource(R.drawable.good_answer);
-                q8a3j.setBackgroundResource(R.drawable.good_answer);
-                q7a.setText(R.string.answer7);
-                Toast.makeText(this, editText1.getText()+ " result is : "+ answer2 +" %", Toast.LENGTH_LONG).show();
-
-
-
-
+        q1a1j.setBackgroundResource(R.drawable.good_answer);
+        q2a2j.setBackgroundResource(R.drawable.good_answer);
+        q3a1j.setBackgroundResource(R.drawable.good_answer);
+        q4a3j.setBackgroundResource(R.drawable.good_answer);
+        q5a3j.setBackgroundResource(R.drawable.good_answer);
+        q6a3j.setBackgroundResource(R.drawable.good_answer);
+        q8a1j.setBackgroundResource(R.drawable.good_answer);
+        q8a2j.setBackgroundResource(R.drawable.good_answer);
+        q8a3j.setBackgroundResource(R.drawable.good_answer);
+        q7a.setText(R.string.answer7);
+        Toast.makeText(this, editText1.getText() + " result is : " + answer2 + " %", Toast.LENGTH_LONG).show();
 
     }
+
     //this one function is to call reset function in android button
-    public void reset1 (View view){
+    public void reset1(View view) {
         reset();
     }
+
     //this one resets every selection and background
     public void reset() {
         jgroup1.clearCheck();
@@ -212,6 +197,6 @@ public class MainActivity extends AppCompatActivity {
         q8a1j.setChecked(false);
         q8a2j.setChecked(false);
         q8a3j.setChecked(false);
+        q8a4j.setChecked(false);
     }
 }
-
